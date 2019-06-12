d52885c4115146605bfd074e229f18307456942c
HawkeDracorr <lilith.hawkedracorr@gmail.com>
2019-06-12 16:34:41 -0500

    Finished prototype.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,13 +1,13 @@
 /*
   Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.
 
   At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
-  
+
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
-  
+
 /*
   === GameObject ===
   * createdAt
@@ -16,12 +16,29 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+function GameObject(chara) {
+  this.createdAt = Date();
+  this.name = chara.name;
+  this.dimensions = chara.dimensions;
+  this.destroy = function() {
+    return `${this.name} was eaten by a grue.`;
+  }
+}
+
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(chara) {
+  GameObject.call(this, chara);
+  this.healthPoints = chara.healthPoints;
+  this.takeDamage = function() {
+    return `${this.name} took damage.`
+  };
+};
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,7 +49,17 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
+function Humanoid(chara) {
+  CharacterStats.call(this, chara);
+  this.team = chara.team;
+  this.weapons = chara.weapons;
+  this.language = chara.language;
+  this.greet = function() {
+    return `${this.name} offers a greeting in ${this.language}`
+  };
+}
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +68,6 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -49,13 +75,14 @@
       width: 1,
       height: 1,
     },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
+    healthPoints: 200,
+    name: 'Moira',
+    team: 'Talon',
     weapons: [
-      'Staff of Shamalama',
+      'Biotic Grasp',
+      'Biotic Orb',
     ],
-    language: 'Common Tongue',
+    language: 'Gaelic',
   });
 
   const swordsman = new Humanoid({
@@ -65,14 +92,14 @@
       width: 2,
       height: 2,
     },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
+    healthPoints: 200,
+    name: 'Genji',
+    team: 'Overwatch',
     weapons: [
-      'Giant Sword',
-      'Shield',
+      'Dragonblade',
+      'Throwing Stars',
     ],
-    language: 'Common Tongue',
+    language: 'Beep boop',
   });
 
   const archer = new Humanoid({
@@ -82,29 +109,69 @@
       width: 2,
       height: 4,
     },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
+    healthPoints: 150,
+    name: 'Hanzo',
+    team: 'Shimada',
     weapons: [
-      'Bow',
       'Dagger',
     ],
-    language: 'Elvish',
+    language: 'Japanese',
   });
 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
 
-  // Stretch task: 
-  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+  // Prints
+  console.log('   ');
+  console.log('The Mage'); // The Mage
+  console.log('________'); //
+  console.log(`dimensions: Length is ${mage.dimensions.length}, width is ${mage.dimensions.width}, height is ${mage.dimensions.height}`); // dimensions of character
+  console.log(`HP: ${mage.healthPoints}`); // Max HP
+  console.log(`Created: ${mage.createdAt}`); // Today's date
+  console.log(`Damaged: ${mage.takeDamage()}`); // Taking Damage
+  console.log(`Name: ${mage.name}`); // Bruce
+  console.log(`Weapon: ${mage.weapons}`); // Staff of Shamalama X
+  console.log(`Greeting: ${mage.greet()}`); //Their Greeting
+  console.log(`Language: ${mage.language}`); // What do they speak X
+  console.log(`Faction: ${mage.team}`); // Guild/faction X
+  console.log(`destroyed: ${mage.destroy()}`);
+
+  console.log('   ');
+  console.log('The Swordsman'); // The swordsman
+  console.log('________'); //
+  console.log(`dimensions: Length is ${swordsman.dimensions.length}, width is ${swordsman.dimensions.width}, height is ${swordsman.dimensions.height}`); // dimensions of character
+  console.log(`HP: ${swordsman.healthPoints}`); // Max HP
+  console.log(`Created: ${swordsman.createdAt}`); // Today's date
+  console.log(`Damaged: ${swordsman.takeDamage()}`); // Taking Damage
+  console.log(`Name: ${swordsman.name}`); // Bruce
+  console.log(`Weapon: ${swordsman.weapons}`); // Staff of Shamalama X
+  console.log(`Greeting: ${swordsman.greet()}`); //Their Greeting
+  console.log(`Language: ${swordsman.language}`); // What do they speak X
+  console.log(`Faction: ${swordsman.team}`); // Guild/faction X
+  console.log(`destroyed: ${swordsman.destroy()}`);
+
+  console.log('   ');
+  console.log('The Archer'); // The archer
+  console.log('________'); //
+  console.log(`dimensions: Length is ${archer.dimensions.length}, width is ${archer.dimensions.width}, height is ${archer.dimensions.height}`); // dimensions of character
+  console.log(`HP: ${archer.healthPoints}`); // Max HP
+  console.log(`Created: ${archer.createdAt}`); // Today's date
+  console.log(`Damaged: ${archer.takeDamage()}`); // Taking Damage
+  console.log(`Name: ${archer.name}`); // Bruce
+  console.log(`Weapon: ${archer.weapons}`); // Staff of Shamalama X
+  console.log(`Greeting: ${archer.greet()}`); //Their Greeting
+  console.log(`Language: ${archer.language}`); // What do they speak X
+  console.log(`Faction: ${archer.team}`); // Guild/faction X
+  console.log(`destroyed: ${archer.destroy()}`);
+
+
+  // console.log(mage.takeDamage()); // Bruce took damage.
+  // console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  // console.log(swordsman.healthPoints); // 15
+  // console.log(swordsman.team); // The Round Table
+  // console.log(archer.language); // Elvish
+  // console.log(archer.greet()); // Liliana offers a greeting in Elvish.
+  // console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+  // Stretch task:
+  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,14 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global: When placed within the global body, the window itself will be the target of `this`
+
+* 2. Implicit: When used within a function, the function is called within itself with the format `this.something`,
+               where `this` is aimed at it's parent function.
+
+* 3. New: when used within a `constructor`, this refers to the specific object created at the time by the `constructor`
+
+* 4. Explicit:
 *
 * write out a code example of each explanation above
 */
